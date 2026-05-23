abde2695ae3ad431b6de78e7e1b01f31ff80922e
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2026-05-23 15:21:57 +0530

    ci: replace incompatible _ss literal with QStringLiteral for Qt 6.4 compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
     engine.loadFromModule("liphis", "Main");
 #else
-    engine.load(QUrl(u"qrc:/qt/qml/liphis/src/qml/Main.qml"_ss));
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/liphis/src/qml/Main.qml")));
 #endif
     if (engine.rootObjects().isEmpty()) return -1;
 
